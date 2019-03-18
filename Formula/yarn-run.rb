7efbd324fcd443b825f2fe827c6d0bfe4510a99e
Markus Maga <markus@maga.se>
2019-03-18 13:31:01 +0100

    fix: yarn-run depends on jq

--- Formula/yarn-run.rb
+++ Formula/yarn-run.rb
@@ -4,8 +4,10 @@ class YarnRun < Formula
   url "https://github.com/Flydiverny/yarn-run/archive/0.2.1.tar.gz"
   sha256 "32e57e652d8ea675cad93a4f7679bd2e7685b64fc7f090c709e18547c6f0bfa6"
   version "0.2.1"
-
+  revision 2
+  
   depends_on 'fzf'
+  depends_on 'jq'
 
   def install
     bin.install "yarn-run"
