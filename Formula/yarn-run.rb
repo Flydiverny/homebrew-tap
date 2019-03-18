a21ec6d1bb68a3ef372341106af7acb5e09767d0
Markus Maga <markus@maga.se>
2019-03-18 14:51:13 +0100

    yarn-run 0.3.0 and added head to formula

--- Formula/yarn-run.rb
+++ Formula/yarn-run.rb
@@ -1,15 +1,16 @@
 class YarnRun < Formula
   desc "fuzzy find enabled yarn run"
   homepage "https://github.com/flydiverny/yarn-run"
-  url "https://github.com/Flydiverny/yarn-run/archive/0.2.1.tar.gz"
-  sha256 "32e57e652d8ea675cad93a4f7679bd2e7685b64fc7f090c709e18547c6f0bfa6"
-  version "0.2.1"
-  revision 2
-  
+  head "https://github.com/Flydiverny/yarn-run.git"
+  url "https://github.com/Flydiverny/yarn-run/archive/0.3.0.tar.gz"
+  sha256 "8f8aa64a508b98248d852e5f01df59efd4f3f4bcfa449e8d43e09d30af931c13"
+  version "0.3.0"
+
   depends_on 'fzf'
   depends_on 'jq'
 
   def install
     bin.install "yarn-run"
+    bin.install "npm-run"
   end
 end
